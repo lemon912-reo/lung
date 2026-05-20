4cc5ae45417fc557b9e414bcdcf0234dbf994f91
lemon912-reo <lemon912@kshcm.net>
2026-05-20 16:39:02 +0700

    Update lung.py

--- lung.py
+++ lung.py
@@ -2,10 +2,9 @@ import streamlit as st
 import pandas as pd
 import joblib
 import matplotlib.pyplot as plt
-import matplotlib.font_manager as fm
 
 # 한글 폰트 설정
-plt.rc('font', family='Malgun Gothic')
+plt.rcParams['font.family'] = 'Malgun Gothic'
 plt.rcParams['axes.unicode_minus'] = False
 
 # =========================
